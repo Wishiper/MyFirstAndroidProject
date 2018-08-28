0260b99e9bea1e1fe481b65795b8f6a58c653817
Wishiper <wishiper@gmail.com>
2018-08-28 17:04:32 +0300

    added project files

---
+++ app/src/main/java/com/example/wishiper/mytelerikproject/NoteAdapter.java
@@ -0,0 +1,25 @@
+//package com.example.wishiper.mytelerikproject;
+//
+//import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+//import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+//import android.support.annotation.NonNull;
+//import android.support.v7.widget.RecyclerView;
+//import android.view.View;
+//import android.widget.TextView;
+//
+//public class NoteAdapter extends FirestoreRecyclerAdapter<Note,NoteAdapter.NoteHolder>{
+//
+//class NoteHolder extends RecyclerView.ViewHolder {
+//    TextView textViewTitle;
+//    TextView textViewDescription;
+//    TextView textViewPriority;
+//
+//
+//    public NoteHolder(@NonNull View itemView) {
+//        super(itemView);
+//        textViewTitle = itemView.findViewById(R.id.text_view_title);
+//        textViewDescription = itemView.findViewById(R.id.text_view_description);
+//        textViewPriority = itemView.findViewById(R.id.text_view_priority)
+//    }
+//}
+//}
